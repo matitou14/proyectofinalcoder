e2935b49c8c7567c81a479cc6f8bba8e3116b2db
Matias Rodriguez <mat.rodri@gmail.com>
2023-07-06 00:35:10 -0300

    mongo db up and dotenv installed

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path, { dirname } from "path";
+import dotenv from 'dotenv'
 import handlebars from "express-handlebars";
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes.js";
@@ -8,9 +9,16 @@ import cartRoutes from "./routes/cartRoutes.js";
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
+dotenv.config();
 const app = express ();
 const port = 8080;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const dbCluster = process.env.DB_CLUSTER;
+
+const dbURI = `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}/${dbName}?retryWrites=true&w=majority`;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,3 +37,6 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected...'))
+  .catch((err) => console.log(err));
